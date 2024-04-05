bfdc6c6454dfe94e3b293840fde8b0ce5f3b6499
LEE MIN GYEONG <70195171+mglee-developer@users.noreply.github.com>
2024-04-05 10:35:09 +0900

    Title: 정수 선언하고 뺄셈, Time: 78ms, Memory: 8MB, Status: Passed - CodeTree

--- 240405/정수 선언하고 뺄셈/define-numbers-and-substract.java
+++ 240405/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -3,6 +3,6 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         int a = 97, b = 13;
         int c = a - b;
-        System.out.print(a + " + " + b + " = " + c);
+        System.out.print(a + " - " + b + " = " + c);
     }
 }
