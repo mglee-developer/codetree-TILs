566be984c8c8b3f48cdf45a1cf0a05221e917896
LEE MIN GYEONG <70195171+mglee-developer@users.noreply.github.com>
2024-04-08 16:41:59 +0900

    Title: 변수 출력하기 3, Time: 106ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240408/변수 출력하기 3/outputing-variables-3.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // 1->2->C
+        int a = 1, b = 2;
+        char c = 'C';
+        System.out.printf("%d->%d->%s", a, b, c);
+    }
+}
