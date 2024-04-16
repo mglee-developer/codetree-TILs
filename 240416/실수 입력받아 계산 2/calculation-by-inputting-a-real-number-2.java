ed6006f19b2a6f6d6ac64d8581290d66323d9583
LEE MIN GYEONG <70195171+mglee-developer@users.noreply.github.com>
2024-04-16 17:03:52 +0900

    Title: 실수 입력받아 계산 2, Time: 151ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240416/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double result = a + 1.5;
+        System.out.printf("%.2f", result);
+    }
+}
